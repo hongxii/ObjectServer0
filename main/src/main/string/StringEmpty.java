1cdd74d926e60fd254f3d6f26cd7710d545efc1f
hongxii <hongxiii@qq.com>
2020-04-15 15:02:55 +0800

    addIni2

---
+++ main/src/main/string/StringEmpty.java
@@ -0,0 +1,14 @@
+package main.string;
+
+/**
+ * @author hongxii
+ */
+public class StringEmpty {
+
+    public static void main(String[] args) {
+
+        String sid = "";
+        System.out.println(sid.isEmpty());
+
+    }
+}
